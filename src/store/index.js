6b2dfb48799c13b799dd27d7104d2fc05825c3a6
Jonathan Gray <jonathanceddy@gmail.com>
2017-02-08 09:32:24 -0800

    added integration with localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+import { loadState, saveState } from './localStorage';
+
 import rootReducer from 'reducers';
 
+const persistedState = loadState();
+
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(rootReducer);
+const store = createStoreWithMiddleware(rootReducer, persistedState);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
