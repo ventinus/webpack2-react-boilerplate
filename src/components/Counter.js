eb0a27ff1954640ec5cf2ce59c8d31ca40facfa6
Jonathan Gray <jonathanceddy@gmail.com>
2017-02-08 12:01:23 -0800

    trimmed syntax in counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,17 +3,14 @@ import { connect } from 'react-redux';
 import { removeCounter, incrementCounter, decrementCounter } from 'actions';
 const { PropTypes } = React;
 
-const Counter = ({onIncrementClick, onDecrementClick, onRemoveClick, index, value}) => {
-
-  return (
-    <div className="counter">
-      <button className="counter__increment" onClick={ onIncrementClick.bind(null, index) } >+</button>
-      <button className="counter__decrement" onClick={ onDecrementClick.bind(null, index) } >-</button>
-      <button className="counter__remove" onClick={ onRemoveClick.bind(null, index) }>Remove Counter</button>
-      <div className="counter__value">{ value }</div>
-    </div>
-  );
-}
+const Counter = ({onIncrementClick, onDecrementClick, onRemoveClick, index, value}) => (
+  <div className="counter">
+    <button className="counter__increment" onClick={ onIncrementClick.bind(null, index) } >+</button>
+    <button className="counter__decrement" onClick={ onDecrementClick.bind(null, index) } >-</button>
+    <button className="counter__remove" onClick={ onRemoveClick.bind(null, index) }>Remove Counter</button>
+    <div className="counter__value">{ value }</div>
+  </div>
+);
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
